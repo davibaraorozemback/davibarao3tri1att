d96c778a00a16683de351c6dc61b9d94ec7feba7
davibaraorozemback <150378504+davibaraorozemback@users.noreply.github.com>
2024-09-24 17:03:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Sociologia',
+    'Oque é Sociologia?',
+    'A Sociologia é a ciência que estuda a sociedade e os fenômenos que nela ocorrem, sejam eles culturais, econômicos, religiosos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da China?',
+    'A capital da China é Pequim '
+)
+
+criaCartao(
+    'Progamaçao',
+    'O que é o GitHub?',
+    'O Git é um sistema de controle de versão distribuído, o que significa que um clone local do projeto é um repositório de controle de versão completo'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz Boa noite, Boa Tarde e Bom dia em Inglês?',
+    'Good night, Good afternoon and Good morning'
+)
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Oque é Substantivo?',
+    'Substantivo é a classe de palavras usada para dar nome aos seres, objetos, fenômenos, lugares, qualidades, ações, dentre outros'
+)
+
+criaCartao(
+    'Historia',
+    'Oque foi a guera dos cem anos?',
+    'A Guerra dos Cem Anos foi uma série de conflitos travados entre os reinos da Inglaterra e da França durante o final da Idade Média'
+)
